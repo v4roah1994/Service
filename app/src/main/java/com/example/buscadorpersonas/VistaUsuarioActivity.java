9fe01b6dc1846020d25f739bb8d4a71d06626e24
PlayerC26 <64438085+PlayerC26@users.noreply.github.com>
2022-11-02 17:36:35 -0500

    Actualizacion de interfaces: Yina Morales

---
+++ app/src/main/java/com/example/buscadorpersonas/VistaUsuarioActivity.java
@@ -0,0 +1,13 @@
+package com.example.buscadorpersonas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VistaUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_usuario);
+    }
+}
